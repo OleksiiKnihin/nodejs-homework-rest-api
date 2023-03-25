e7be54cc632e2fe65b01e7d9b440a063b2b4148f
Aleksey Knigin <alexyo2626@gmail.com>
2023-03-25 20:58:43 +0200

    add hw06-email

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,9 +1,12 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
+const { v4: uuidv4 } = require("uuid");
 
 const { User } = require("../../models/user");
 
-const { HttpError } = require("../../helpers");
+const { BASE_URL } = process.env;
+
+const { HttpError, sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -15,12 +18,22 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationCode = uuidv4();
 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationCode,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationCode}">Click verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     name: newUser.name,
